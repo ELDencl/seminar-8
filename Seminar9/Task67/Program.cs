b49974c84240498432015e306aee20534b6032dd
Denis <Den00736@mail.ru>
2023-03-02 01:33:49 +0400

    Seminar 9

---
+++ Seminar9/Task67/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и 
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+
+
+int SumRec(int n)
+{
+    if (n == 0) return 0;
+    else return n % 10 + SumRec(n / 10);
+
+
+}
+
+
+
+
+
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write(SumRec(n));
---
+++ Seminar9/Task69/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и 
+// возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8 
+
+
+
+
+int PowerNum(int a, int b)
+{
+    if (b == 0) return 1;
+    else return a * PowerNum(a, b - 1);
+}
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите степень: ");
+int b = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(PowerNum(a,b));
